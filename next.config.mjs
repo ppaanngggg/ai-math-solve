97e2a19edaf8015064891b13d450a29e5660ad2d
ppaanngggg <hantian.pang@gmail.com>
2024-06-20 10:16:02 +0800

    add next config

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '10mb'
+        }
+    }
+}
+ 
+export default nextConfig
